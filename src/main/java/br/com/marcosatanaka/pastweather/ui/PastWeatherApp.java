e93131de6da4b584daf1291106be3d25eb273e53
Marcos Tanaka <marcosatanaka@gmail.com>
2015-06-14 19:47:26 -0300

    Starting to create the user interface with JavaFX.

---
+++ src/main/java/br/com/marcosatanaka/pastweather/ui/PastWeatherApp.java
@@ -0,0 +1,21 @@
+package br.com.marcosatanaka.pastweather.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Application main class which has the start() method, the main entry point for all JavaFX applications.
+ */
+public class PastWeatherApp extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(PastWeatherApp.class, args);
+    }
+
+}
